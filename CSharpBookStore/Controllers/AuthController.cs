3050f5f5047e818e02d0d628a86e4af7390c1545
ApramPepo <pepoapram2016@gmail.com>
2026-03-15 00:48:11 +0200

    Cleaned some useless requests and added Swagger UI

--- CSharpBookStore/Controllers/AuthController.cs
+++ CSharpBookStore/Controllers/AuthController.cs
@@ -6,7 +6,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Microsoft.AspNetCore.Authorization;
 
 namespace CSharpBookStore.Controller;
 
--- CSharpBookStore/Program.cs
+++ CSharpBookStore/Program.cs
@@ -4,9 +4,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.IdentityModel.Tokens.Experimental;
-using Microsoft.AspNetCore.OpenApi;
 using System.Text;
+using Microsoft.OpenApi;
 
 //**********
 // Config 
@@ -15,9 +14,17 @@ var builder = WebApplication.CreateBuilder(args);
 var JwtSettings = builder.Configuration.GetSection("Jwt");
 var key = Encoding.UTF8.GetBytes(JwtSettings["Key"]!);
 
+
+//**********
+// Services
+//**********
 builder.Services.AddControllers();
 builder.Services.AddDbContext<BookstoreDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>().AddEntityFrameworkStores<BookstoreDbContext>().AddDefaultTokenProviders();
+
+//**********
+// Auth
+//**********
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
 {
     options.TokenValidationParameters = new TokenValidationParameters
@@ -32,19 +39,56 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJw
     };
 });
 builder.Services.AddAuthorization();
-builder.Services.AddOpenApi();
 
+//**********
+// Swagger
+//**********
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info ??= new OpenApiInfo
+        {
+            Title = "CSharpBookStore Api",
+            Version = "v1",
+            Description = "An API for managing a bookstore, including books, authors, and user accounts.",
+        };
+
+        document.Components ??= new OpenApiComponents();
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+        document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+        {
+            Description = "JWT Authorization header using the Bearer scheme.",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            Scheme = "Bearer",
+            BearerFormat = "JWT"
+        };
+
+        
+        return Task.CompletedTask;
+    });
+});
+
+//**********
+// Middleware & Build
+//**********
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
-
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "CSharpBookStore API V1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
+app.MapControllers();
 app.Run();
